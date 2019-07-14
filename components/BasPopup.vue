a175b97cf5d256c6618c8409502118cba3e515ac
basked <basket.baza@gmail.com>
2019-07-14 22:30:39 +0300

    first commit

---
+++ components/BasPopup.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="popup">
+        <h1>Employees</h1>
+
+        <ul>
+            <li>basked1</li>
+            <li>basked2</li>
+            <li>basked3</li>
+            <li>basked4</li>
+            <li>basked5</li>
+        </ul>
+        <dx-popup :visible.sync="popupVisible"
+                  :drag-enabled="false"
+                  :close-on-outside-click="true"
+                  :show-title="true"
+                  :width="300"
+                  :height="250"
+                  class="popup"
+                  title="Information"
+        >
+        </dx-popup>
+    </div>
+</template>
+
+<script>
+    import {DxPopup} from 'devextreme-vue/popup';
+
+    export default {
+        name: "BasPopup",
+        components: {
+            DxPopup
+        },
+        props: {
+            propsPopupVisible: Boolean
+        },
+        data() {
+            return {
+                popupVisible:false
+            }
+        },
+
+        watch: {
+            propsPopupVisible: function (newVal, oldVal) { // watch it
+
+              console.log(  this.popupVisible);
+              console.log(newVal);
+              console.log( oldVal);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #popup {
+        padding: 10px;
+    }
+
+    #popup ul {
+        list-style-type: none;
+        text-align: center;
+    }
+
+    #popup ul li {
+        display: inline-block;
+        width: 160px;
+        margin: 10px;
+    }
+
+    #popup ul li img {
+        width: 100px;
+    }
+
+    .button-info {
+        margin: 10px;
+    }
+
+    .popup p {
+        margin-bottom: 10px;
+        margin-top: 0;
+    }
+</style>
---
+++ components/BasToolBar.vue
@@ -0,0 +1,128 @@
+<template>
+    <div>
+        <bas-popup :propsPopupVisible.sync="isPopupVisible"></bas-popup>
+        <dx-toolbar :items="items"/>
+        <dx-list
+                id="products"
+                :data-source="productsStore"
+        />
+    </div>
+</template>
+<script>
+    import 'devextreme/dist/css/dx.common.css';
+    import 'devextreme/dist/css/dx.material.teal.light.compact.css';
+
+
+    import DxToolbar from 'devextreme-vue/toolbar';
+    import DxList from 'devextreme-vue/list';
+
+    import DataSource from 'devextreme/data/data_source';
+    import notify from 'devextreme/ui/notify';
+    import 'devextreme/ui/select_box';
+    import { productTypes, products } from '@/store/DevData/ToolBar.js';
+    import BasPopup from '@/components/BasPopup'
+
+    export default {
+        name:"BasToolBar",
+        components: {
+            DxToolbar,
+            DxList,
+            BasPopup
+        },
+        data() {
+            return {
+                isPopupVisible: false,
+                productsStore: new DataSource(products),
+                items: [{
+                    location: 'before',
+                    widget: 'dxButton',
+                    options: {
+                        type: 'back',
+                        text: 'Back',
+                        onClick: (e) => {
+                            this.isPopupVisible =true;
+                            notify('Back button has been clicked!');
+                        }
+                    }
+                }, {
+                    location: 'before',
+                    widget: 'dxButton',
+                    locateInMenu: 'auto',
+                    options: {
+                        icon: 'refresh',
+                        onClick: () => {
+                            notify('Refresh button has been clicked!');
+                        }
+                    }
+                }, {
+                    location: 'center',
+                    locateInMenu: 'never',
+                    template: () => {
+                        return '<div class=\'toolbar-label\'><b>Tom\'s Club</b> Products</div>';
+                    }
+                }, {
+                    location: 'after',
+                    widget: 'dxSelectBox',
+                    locateInMenu: 'auto',
+                    options: {
+                        width: 140,
+                        items: productTypes,
+                        valueExpr: 'id',
+                        displayExpr: 'text',
+                        value: productTypes[0].id,
+                        onValueChanged: (args) => {
+                            if (args.value > 1) {
+                                this.productsStore.filter('type', '=', args.value);
+                            } else {
+                                this.productsStore.filter(null);
+                            }
+                            this.productsStore.load();
+                        }
+                    }
+                }, {
+                    location: 'after',
+                    widget: 'dxButton',
+                    locateInMenu: 'auto',
+                    options: {
+                        icon: 'plus',
+                        onClick: () => {
+
+                            notify('Add button has been clicked!');
+                        }
+                    }
+                }, {
+                    locateInMenu: 'always',
+                    text: 'Save',
+                    onClick: () => {
+                        notify('Save option has been clicked!');
+                    }
+                }, {
+                    locateInMenu: 'always',
+                    text: 'Print',
+                    onClick: () => {
+                        notify('Print option has been clicked!');
+                    }
+                }, {
+                    locateInMenu: 'always',
+                    text: 'Settings',
+                    onClick: () => {
+                        notify('Settings option has been clicked!');
+                    }
+                }]
+            };
+        },
+        methods:{
+
+        }
+    };
+</script>
+<style>
+    .toolbar-label,
+    .toolbar-label > b {
+        font-size: 16px;
+    }
+
+    #products {
+        margin-top: 10px;
+    }
+</style>
