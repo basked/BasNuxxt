23409ac93ec95c323a0196b629f78e14badd02b2
basked <basket.baza@gmail.com>
2019-07-15 15:22:29 +0300

    Настроены компоненты: -BasPopup  (модольное оно с выполненными задачами) -BasProgresBar (вызывает прогрсесс выполненной задачи) -BasToolBar  (вызывает BasPopup)

--- components/BasPopup.vue
+++ components/BasPopup.vue
@@ -14,18 +14,42 @@
         >
             <dx-scroll-view :height="240">
                 <ul>
-                    <li><bas-progress-bar :propsMaxValue="60" :propsCurValue="16"/></li>
-                    <li><bas-progress-bar :propsMaxValue="30" :propsCurValue="20"/></li>
-                    <li><bas-progress-bar :propsMaxValue="30" :propsCurValue="10"/></li>
-                    <li><bas-progress-bar :propsMaxValue="40" :propsCurValue="50"/></li>
-                    <li><bas-progress-bar :propsMaxValue="50" :propsCurValue="10"/></li>
-                    <li><bas-progress-bar :propsMaxValue="30" :propsCurValue="10"/></li>
-                    <li><bas-progress-bar :propsMaxValue="60" :propsCurValue="16"/></li>
-                    <li><bas-progress-bar :propsMaxValue="30" :propsCurValue="20"/></li>
-                    <li><bas-progress-bar :propsMaxValue="35" :propsCurValue="20"/></li>
-                    <li><bas-progress-bar :propsMaxValue="45" :propsCurValue="10"/></li>
-                    <li><bas-progress-bar :propsMaxValue="70" :propsCurValue="50"/></li>
-                    <li><bas-progress-bar :propsMaxValue="80" :propsCurValue="10"/></li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="60" :propsCurValue="curValue"/>
+                    </li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="30" :propsCurValue="20"/>
+                    </li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="30" :propsCurValue="10"/>
+                    </li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="40" :propsCurValue="50"/>
+                    </li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="50" :propsCurValue="10"/>
+                    </li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="30" :propsCurValue="10"/>
+                    </li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="60" :propsCurValue="16"/>
+                    </li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="30" :propsCurValue="20"/>
+                    </li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="35" :propsCurValue="20"/>
+                    </li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="45" :propsCurValue="10"/>
+                    </li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="70" :propsCurValue="50"/>
+                    </li>
+                    <li>
+                        <bas-progress-bar :propsMaxValue="80" :propsCurValue="10"/>
+                    </li>
                 </ul>
             </dx-scroll-view>
         </dx-popup>
@@ -48,6 +72,8 @@
         props: {propsPopupVisible: Boolean, requre: true},
         data() {
             return {
+                curValue: 0,
+                inProgress: false,
                 popupVisible: false
             }
         },
@@ -59,6 +85,11 @@
         methods: {
             onShowing() {
                 this.$emit('showPopup', this.popupVisible);
+                this.intervalId = setInterval(() => this.timer(), 1000);
+            },
+            timer() {
+              this.curValue= this.curValue+1;
+              console.log(this.curValue);
             },
             onHiding() {
                 this.$emit('hidePopup', this.popupVisible);
@@ -80,6 +111,7 @@
         color: gainsboro;
         background-color: palegreen;
         margin-bottom: 10px;
+        font-size: small;
     }
 
 
